bdd727075d7895539a4a87c15314633f287c2dbc
creaciond <daria.maximova.m@gmail.com>
2016-08-17 19:34:34 +0200

    add code
    UnicodeDecodeError на большинстве томов, хотя на 59, 61, 70, 71, 72
    переименование сработало

---
+++ renaming.py
@@ -0,0 +1,31 @@
+# Переименовываем письма в папках
+
+# encoding: utf-8 -*- 
+import os
+import re
+import sys
+
+	
+def renamedir(dirname):
+    # открыть каждый, найти название (тег text), переименовать,закрыть
+    for each in os.listdir(dirname):
+        letterpath = dirname+'/'+each
+        letter = open(letterpath, 'r', encoding='utf-8')
+        for line in letter:
+            reg = re.search('<text xml:id=\"(.*?)\"', line)
+            if reg:
+                name = reg.group(1)
+                os.rename(letterpath, dirname+'/'+name+".xml")
+        letter.close()
+    
+
+def main():
+    for i in range(59, 89):
+        numstr = str(i)
+        renamedir("Volume_"+numstr)
+        # как только всё переименовано,маякнуть об этом
+        print(numstr)   
+
+
+if __name__ == '__main__':
+	main()
